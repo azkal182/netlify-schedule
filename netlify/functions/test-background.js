027248ef1197aaa1ba70ace6c385cc7cb593d7a2
azka <mohazkalarif@gmail.com>
2023-01-08 20:47:39 +0700

    init

--- netlify/functions/test-background.js
+++ netlify/functions/test-background.js
@@ -2,7 +2,7 @@
 const token = "5730954289:AAGDPIDwIyhae6aQwb7Y4T7_Gf6PrhyPd30";
 const chatId = "404000198";
 const https = require("https");
-const { schedule } = require("@netlify/functions");
+const { schedule, BackgroundHandler } = require("@netlify/functions");
 
 const handler = async function(event, context) {
     console.log("Received event:");
