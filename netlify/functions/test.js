1f8d1ec47d8cf1496a3f8197ddf24cb8213525f5
azka <mohazkalarif@gmail.com>
2023-01-08 20:27:48 +0700

    init

--- netlify/functions/test.js
+++ netlify/functions/test.js
@@ -1,13 +1,39 @@
 // YOUR_BASE_DIRECTORY/netlify/functions/test-scheduled-function.js
+const token = "5730954289:AAGDPIDwIyhae6aQwb7Y4T7_Gf6PrhyPd30";
+const chatId = "404000198";
 
 const { schedule } = require("@netlify/functions");
 
 const handler = async function(event, context) {
     console.log("Received event:");
+    sendMessage("schedule trigered");
 
     return {
         statusCode: 200,
     };
 };
+
+async function sendMessage(messages) {
+ const message = encodeURIComponent(messages);
+ const options = {
+  hostname: "api.telegram.org",
+  port: 443,
+  path: `/bot${token}/sendMessage?chat_id=${chatId}&text=${message}`,
+  method: "POST",
+ };
+ const req = https.request(options, (res) => {
+  //console.log(`statusCode: ${res.statusCode}`);
+
+  res.on("data", (d) => {
+   process.stdout.write(d);
+  });
+ });
+
+ req.on("error", (error) => {
+  console.error(error);
+ });
+
+ req.end();
+}
 
 exports.handler = schedule("* * * * *", handler);
